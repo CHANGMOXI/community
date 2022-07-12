96a949df4a327a8705b1adb470ca1cf2c75bae8d
javagit <2143204964@qq.com>
2022-07-13 01:33:37 +0800

    实现持久化登录状态

--- src/main/java/life/majiang/community/dao/UserDao.java
+++ src/main/java/life/majiang/community/dao/UserDao.java
@@ -2,8 +2,9 @@ package life.majiang.community.dao;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import life.majiang.community.domain.User;
-import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 /**
  * @author CZS
@@ -14,4 +15,8 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface UserDao extends BaseMapper<User> {
     //与整合MyBatis的不同：继承BaseMapper，指定泛型后，就带有基本的CURD操作的方法
+
+    //自定义查询
+    @Select("select * from user where token = #{token};")
+    User findByToken(@Param("token") String token);
 }
